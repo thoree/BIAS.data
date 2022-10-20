a40e496f15f9f65d7676de89cc98f43a08aede28
Kristin K Solberg <kristin.kleivan.solberg@nmbu.no>
2022-10-20 08:23:00 +0200

    Deleting several files in the data-raw folder (related to removed data sets)

--- data-raw/sire.R
+++
@@ -1,2 +0,0 @@
-load("C:\\Users\\theg\\Dropbox\\umb\\stat340\\2020\\lectures\\data\\sire.Rdata")
-usethis::use_data(sire, overwrite = TRUE)
--- data-raw/small.R
+++
@@ -1,2 +0,0 @@
-load("C:\\Users\\theg\\Dropbox\\umb\\stat340\\2020\\lectures\\data\\small.Rdata")
-usethis::use_data(small, overwrite = TRUE)
