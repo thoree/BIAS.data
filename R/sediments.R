b849cfbf72fc96dbfc445fad2d146b72c76a6820
Lars Snipen <lars.snipen@nmbu.no>
2023-01-25 10:30:28 +0100

    Updated data sets sediments and Hald.Cement

--- R/sediments.R
+++ R/sediments.R
@@ -1,7 +1,7 @@
 #' sediments: Prokaryotic Composition in Seafloor Sediments
 #'
 #' This data is from AQUAeD, a project at NMBU. Seafloor sediments from 12
-#' different sites along the Norwegian coast have been collected. Their
+#' selected sites along the Norwegian coast have been collected. Their
 #' prokaryotic composition were estimated by amplicon sequencing and subsequent
 #' bioinformatic analysis.
 #'
@@ -10,13 +10,13 @@
 #' \tabular{llllr}{
 #'   \tab \bold{Column name} \tab \bold{Data type} \tab \bold{Description} \tab \bold{Values}\cr
 #'   \code{[,1]} \tab \code{Environment} \tab character \tab Samples from 2 types of seafloor sediments \tab (Polluted, Natural)\cr
-#'   \code{[,2:4113]} \tab \code{Aaosphaeria...Zymoseptoria} \tab numeric \tab Abundance of the various prokaryotic genera \tab (-0.000242559 - 0.3528014)
+#'   \code{[,2:4113]} \tab \code{Aaosphaeria...Zymoseptoria} \tab numeric \tab Abundance of the various prokaryotic genera \tab (1 - 1475584)
 #' }
 #'
-#' The columns \code{sediments[, 2:4113]} are centered log-transformed relative
-#' abundances of various prokaryotic genera. A large value means a larger
-#' abundance, i.e., the most common genera have the largest values, and vice
-#' versa.
+#' The columns \code{sediments[, 2:4113]} are actual read-counts for various
+#' prokaryotic genera. A larger value means a larger abundance, but sequencing
+#' depth (total read-count) varies between the samples (rows).
+#' See \code{\link{sediments}} for the CLR-transformed data.
 #'
 #' @examples
 #'
@@ -25,10 +25,10 @@
 #'
 #' # Boxplots for the abundance of Acephala and Aeromonas
 #' par(mfrow = c(1, 2))
-#' boxplot(Acephala ~ Environment, data = sediments,
+#' boxplot(Acephala ~ Environment, data = sediments_raw,
 #'         col = c("sienna4", "tomato1"),
 #'         main = "Acephala", ylab = "Abundance")
-#' boxplot(Aeromonas ~ Environment, data = sediments,
+#' boxplot(Aeromonas ~ Environment, data = sediments_raw,
 #'         col = c("sienna4", "tomato1"),
 #'         main = "Aeromonas", ylab = "Abundance")
 #'
