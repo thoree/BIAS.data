f66a13400261a29a5dbc5fcb00d3040280238cb8
Lars Snipen <lars.snipen@nmbu.no>
2023-01-27 11:44:17 +0100

    Corrected spelling in riverfishing

--- R/riverfishing.R
+++ R/riverfishing.R
@@ -1,7 +1,7 @@
-#' riverfishing: Catches of salmon and seatrout in Norway in three periodes
+#' riverfishing: Catches of salmon and seatrout in Norway in three periods
 #'
 #' Catches from river-fishing in Norway, summarised for all rivers in four regions,
-#' and from three distinct 5-year periodes.
+#' and from three distinct 5-year periods.
 #'
 #' @format A data frame with 120 observations (rows) and 4 variables (columns).
 #' \tabular{llllr}{
@@ -9,13 +9,13 @@
 #'   \code{[,1]} \tab \code{Region} \tab character \tab Regions in Norway \tab ("Southern", "Western", "Mid", "Northern")\cr
 #'   \code{[,2]} \tab \code{Species} \tab character \tab Fishh species \tab ("20 - 255"ssalmon", "Seatrout")\cr
 #'   \code{[,3]} \tab \code{Catches} \tab integer \tab Catches in kg \tab (918 - 157593)\cr
-#'   \code{[,4]} \tab \code{Periode} \tab character \tab 5-year periodes \tab ("1993-97", "2005-09", "2018-22")\cr
+#'   \code{[,4]} \tab \code{Period} \tab character \tab 5-year periods \tab ("1993-97", "2005-09", "2018-22")\cr
 #' }
 #'
 #' @examples
 #'
 #' # Linear model
-#' res <- lm(Catchest ~ Region + Species + Periode, data = riverfishing)
+#' res <- lm(Catchest ~ Region + Species + Period, data = riverfishing)
 #'
 #' # Box plot of catches in regions
 #' boxplot(Catches ~ Regions, data = riverfishing)
