d795e07f001967727406c89951b717a543d4b8b3
thoree <thore.egeland@gmail.com>
2024-01-29 15:22:17 +0100

    Added general function for data

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' small data
+#'
+#' A very small data used to illustrate logistic regression.
+#'
+#' @format
+#'
+#'    * `y`: Binary numeric response, 0 or 1.
+#'    * `x`: Predictor variable, 0 or 1.
+#'
+#' @examples
+#'
+#' res1 <- glm(y ~ x, data = small, family = binomial)
+#' coef(res1)
+#'
+"small"
